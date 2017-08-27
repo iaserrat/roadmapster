6dda0a98baa6295ece5daea6cfd4894cd28a2f36
Ismael Serratos <ismael.serratos@shopify.com>
2017-08-27 11:17:14 -0400

    Create tests for Organizations

---
+++ spec/roadmapster_wizeline_api_organization_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'vcr'
+
+RSpec.describe Roadmapster::Wizeline::Organization do
+
+  before :each do
+    @api_token = ENV['WIZELINE_API_TOKEN']
+    @organizations = Roadmapster::Wizeline::Organization.new(token: @api_token)
+  end
+
+  vcr_options = { cassette_name: 'wizeline/get_organizations' }
+  it 'list all organizations for the current user', vcr: vcr_options do
+    organizations = @organizations.all
+    expect(organizations[:data]).to be_an(Array)
+  end
+
+  vcr_options = { cassette_name: 'wizeline/get_organization' }
+  it 'finds an organization by id', vcr: vcr_options do
+    organization = @organizations.find('M_d4XCCpQ32XQosuGZGAiw')
+    expect(organization).to be_an(Hash)
+  end
+end
